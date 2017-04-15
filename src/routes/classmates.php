82e7e53628e424053097500b5788d94da8a45ed8
Shalvah <diakon.ng@gmail.com>
2017-04-15 18:57:19 +0100

    Select only verified users

--- src/routes/classmates.php
+++ src/routes/classmates.php
@@ -6,7 +6,7 @@ use \Psr\Http\Message\ResponseInterface as Response;
 $dbh = require_once __DIR__.'\..\db\init.php';
 
 $app->get('/classmates', function (Request $request, Response $response) use ($dbh) {
-    $result = Db::table("classmates")->select()->get();
+    $result = Db::table("classmates")->select()->where([["is_verified", "=", true]])->get();
 
     $data = [];
     foreach ($result as $row) {
@@ -22,10 +22,12 @@ $app->get('/classmates', function (Request $request, Response $response) use ($d
 
 $app->get('/classmates/{id}', function (Request $request, Response $response) use ($dbh) {
     $id = $request->getAttribute("id");
-    $result = Db::table("classmates")->select()->where([["id", "=", $id]])->get();
-
-    $data = array_except($result[0], ["id", "password", "created_at", "updated_at"]);
-
+    $result = Db::table("classmates")->select()
+        ->where([["id", "=", $id], ["is_verified", "=", true]])->get();
+    $data = [];
+    if(isset($result[0])) {
+        $data = array_except($result[0], ["id", "password", "created_at", "updated_at"]);
+    }
     $response->getBody()->write(json_encode($data));
 
     return $response->withHeader("Content-Type", "application/json");
@@ -33,6 +35,7 @@ $app->get('/classmates/{id}', function (Request $request, Response $response) us
 
 $app->post('/classmates', function (Request $request, Response $response) use ($dbh) {
     $body = $request->getParsedBody();
+    $body["is_verified"] = false;
     $id = Db::table("classmates")->insert($body)->run();
 
     $response->getBody()->write(json_encode($id));
