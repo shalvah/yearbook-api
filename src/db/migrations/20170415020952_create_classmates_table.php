3dc592101999a53c86f0d2933af39aafd3077fbb
Shalvah <diakon.ng@gmail.com>
2017-04-15 03:26:36 +0100

    Set up classmates table

--- src/db/migrations/20170415020952_create_classmates_table.php
+++ src/db/migrations/20170415020952_create_classmates_table.php
@@ -27,6 +27,32 @@ class CreateClassmatesTable extends AbstractMigration
      */
     public function change()
     {
+        $classmates = $this->table('classmates');
+        $classmates->addColumn('email', 'string', ['limit' => 100])
+            ->addColumn('password', 'string', ['limit' => 40])
+            ->addColumn('name', 'string', ['limit' => 90])
+            ->addColumn('is_verified', 'bool', ['default' => 0])
 
+            ->addColumn('phone', 'string', ['limit' => 17, 'null' => true])
+            ->addColumn('photo_url', 'string', ['limit' => 80, 'null' => true])
+            ->addColumn('hobbies', 'string', ['limit' => 50, 'null' => true])
+            ->addColumn('passion', 'string', ['limit' => 50, 'null' => true])
+            ->addColumn('fav_quote', 'string', ['limit' => 120, 'null' => true])
+            ->addColumn('fav_quote_author', 'string', ['limit' => 30, 'null' => true])
+
+            ->addColumn('whatsapp', 'string', ['limit' => 17, 'null' => true])
+            ->addColumn('facebook', 'string', ['limit' => 40, 'null' => true])
+            ->addColumn('twitter', 'string', ['limit' => 30, 'null' => true])
+            ->addColumn('linkedin', 'string', ['limit' => 50, 'null' => true])
+            ->addColumn('instagram', 'string', ['limit' => 30, 'null' => true])
+            ->addColumn('snapchat', 'string', ['limit' => 30, 'null' => true])
+            ->addColumn('website', 'string', ['limit' => 50, 'null' => true])
+            ->addColumn('blog', 'string', ['limit' => 50, 'null' => true])
+
+            ->addColumn('created', 'datetime')
+            ->addColumn('updated', 'datetime', ['null' => true])
+
+            ->addIndex(['email'], ['unique' => true])
+            ->create();
     }
 }
