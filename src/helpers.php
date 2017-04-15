fa7ad9a1374c082c8c25fe804fca9a03dbef4285
Shalvah <diakon.ng@gmail.com>
2017-04-15 13:04:09 +0100

    Add helpers

---
+++ src/helpers.php
@@ -0,0 +1,30 @@
+<?php
+
+if(!function_exists("env")) {
+    function env($key, $default = "")
+    {
+        $value = getenv($key);
+        if($value) {
+            return $value;
+        }
+        return $default;
+    }
+}
+
+if(!function_exists("config")) {
+    function config($keyString, $default = "")
+    {
+        $keys = explode(".", $keyString);
+        $config = require_once("config.php");
+
+        $value = $config;
+        foreach($keys as $key) {
+            if(isset($value[$key])) {
+                $value = $value[$key];
+            } else {
+                return $default;
+            }
+        }
+        return $value;
+    }
+}
